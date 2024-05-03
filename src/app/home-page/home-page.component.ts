0bdcde8984f470f1f29ea4ff02c42b7d458d2624
Jayani Sumudini <jayani@rhino-partners.com>
2024-05-03 21:27:37 +0530

    home screen data table

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Comment } from '../models/comment';
+import { getAllCommentsResponse, saveCommentResponse } from '../models/response';
 import { CommentService } from '../services/comment.service';
 
 @Component({
@@ -9,7 +11,7 @@ import { CommentService } from '../services/comment.service';
 })
 export class HomePageComponent implements OnInit {
   public commentForm!: FormGroup;
-  public allComments: any = [];
+  public allComments: Comment[] = [];
 
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
 
@@ -19,17 +21,22 @@ export class HomePageComponent implements OnInit {
     this.commentForm = new FormGroup({
       comment: new FormControl('', [Validators.required]),
     });
-    this.commentService.getAllComments().subscribe((comments) => {
-      this.allComments = comments;
+    this.commentService.getAllComments().subscribe((response: getAllCommentsResponse) => {
+      this.allComments = response.comments;
     });
   }
 
   public onSubmit() {
     console.log(this.commentForm.get('comment')!.value);
     this.commentService.saveComment(this.commentForm.get('comment')!.value).subscribe({
-      next: (res) => {
-        this.commentService.getAllComments().subscribe((comments) => {
-          this.allComments = comments;
+      next: (response: saveCommentResponse) => {
+        this.commentService.getAllComments().subscribe({
+          next: (response: getAllCommentsResponse) => {
+            this.allComments = response.comments;
+          },
+          error: (err) => {
+            console.log(err);
+          },
         });
       },
       error: (err) => {
--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { getAllCommentsResponse, saveCommentResponse } from '../models/response';
 
 @Injectable({
   providedIn: 'root',
@@ -9,13 +11,13 @@ import { environment } from '../../environments/environment';
 export class CommentService {
   constructor(private http: HttpClient, private router: Router) {}
 
-  public saveComment(comment: string) {
-    return this.http.post(environment.apiUrl + '/comments', {
+  public saveComment(comment: string): Observable<saveCommentResponse> {
+    return this.http.post<saveCommentResponse>(environment.apiUrl + '/comments', {
       content: comment,
     });
   }
 
-  public getAllComments() {
-    return this.http.get(environment.apiUrl + '/comments');
+  public getAllComments(): Observable<getAllCommentsResponse> {
+    return this.http.get<getAllCommentsResponse>(environment.apiUrl + '/comments');
   }
 }
